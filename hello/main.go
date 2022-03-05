0e9c39fef3ec4f63df6990c99c861f89a67e66fc
Fremonster <melanie.shaffer@gmail.com>
2022-03-04 16:26:42 -0800

    Initial practice exercises

---
+++ hello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+// lowercase function = private, uppercase = public
+func main() {
+	fmt.Println("Hello from Go!")
+}
---
+++ practice/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+const aConst int = 64
+
+func main() {
+
+	// explicit typing
+	var aString string = "This is Go!!!"
+	fmt.Println(aString)
+	fmt.Printf("The variable's type is %T\n", aString)
+
+	var anInteger int = 42
+	fmt.Println(anInteger)
+	fmt.Printf("The variable's type is %T\n", anInteger)
+
+	var defaultInt int // defaults to 0
+	fmt.Println(defaultInt)
+
+	// implicit typing
+	var anotherString = "This is another string"
+	fmt.Println(anotherString)
+	fmt.Printf("The variable's type is %T\n", anotherString)
+
+	var anotherInt = 53
+	fmt.Println(anotherInt)
+	fmt.Printf("The variable's type is %T\n", anotherInt)
+
+	// := operator - only works inside of functions
+	myString := "This is also a string"
+	fmt.Println(myString)
+	fmt.Printf("The variable's type is %T\n", myString)
+
+	// constants - defined outside of the function
+	fmt.Println(aConst)
+	fmt.Printf("The variable's type is %T\n", aConst)
+
+}
